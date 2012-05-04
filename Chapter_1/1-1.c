2a504703ca5f23b83b22d08e8c13785c0503483e
Yi Yang <cloverstd@gmail.com>
2012-05-04 23:32:18 +0800

    Update Chapter_1/1-1.c

--- Chapter_1/1-1.c
+++ Chapter_1/1-1.c
@@ -1,5 +1,6 @@
 /*Run the "hello, world" program on your system. Experiment with leaving out parts of the program, to see what error messages you get.
-http://clc-wiki.net/wiki/K%26R2_solutions:Chapter_1:Exercise_1
+
+ Example: http://clc-wiki.net/wiki/K%26R2_solutions:Chapter_1:Exercise_1
 */
 
 #include<stdio.h>
