2f484469507a17f5aedfb53f23da6d74cd4b6e1b
CloverStd <cloverstd@gmail.com>
2012-05-06 12:33:08 +0800

    1-14.c

--- Chapter_1/1-14.c
+++ Chapter_1/1-14.c
@@ -2,4 +2,48 @@
  */
 
 #include<stdio.h>
+#include<ctype.h>
+#define MAXLEN 127
+main()
+{
+	int c;
+	int thisval, maxval = 0;
+	int chararr[MAXLEN + 1];
+	int i;
+	for (i = 0; i <= MAXLEN; i++)
+		chararr[i] = 0;
+	while ((c = getchar()) != EOF)
+	{
+		thisval = ++chararr[c];
+		if (thisval > maxval)
+			maxval = thisval;
+	}
+	int j;
+	for (i = maxval; i > 0; i--)
+	{
+		printf("%4d|", i);
+		for (j = 0; j <= MAXLEN; j++)
+			if (chararr[j] != 0)		// 当字符频率次数为0时，不输出
+				if (chararr[j] >= i)
+					printf("*");
+				else
+					printf(" ");
+		printf("\n");
+	}
+	printf("    +");
+	for (i = 0; i <= MAXLEN; i++)
+		if (chararr[i] != 0)
+			printf("-");
+	printf("\n     ");
+	for (i = 0; i <= MAXLEN; i++)
+	{
+		if (chararr[i] != 0)
+		{
+			if (isprint(i))
+				printf("%c", i);
+			else
+				printf(" ");
+		}
+	}
 
+}
