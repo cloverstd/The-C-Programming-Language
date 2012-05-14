d1540fc2a735a6373ccfc44432028173cbbd4c06
CloverStd <cloverstd@gmail.com>
2012-05-14 21:44:07 +0800

    2-7.c

---
+++ Chapter_2/2-7.c
@@ -0,0 +1,7 @@
+/* Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted
+ * (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
+ */
+unsigned invert(unsigned x, int p, int n)
+{
+	return x ^ (~(~0 << n) << (p + 1 -n));
+}
