57c80667cca3b69c81af5a31df66eeb65a81913c
CloverStd <cloverstd@gmail.com>
2012-05-17 21:37:05 +0800

    3-4.c

---
+++ Chapter_3/3-4.c
@@ -0,0 +1,5 @@
+/* In a two's complement number representation, our version of itoa does not handle the largest
+ * negative number, that is, the value of n equal to -(2 to the power (wordsize - 1)) . Explain why
+ * not. Modify it to print that value correctly regardless of the machine on which it runs.
+ */
+
