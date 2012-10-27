ae911e8a990056881fafab354aa66c3e8f51ed83
cloverstd <cloverstd@gmail.com>
2012-10-28 07:54:36 +0800

    maybe wrong

---
+++ c_point/7-11-4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int max_list(int n, ...);
+
+int main(void)
+{
+    printf("%d\n", max_list(5, 4, 5, 6, -1));
+    return EXIT_SUCCESS;
+}
+
+int max_list(int n, ...)
+{
+    va_list var_arg;
+    int integer;
+    int ii;
+    int max;
+    va_start(var_arg, n);
+
+    max = va_arg(var_arg, int); // get the first integer
+    integer = max;
+    for (ii = 0; ii < n; ii++)
+    {
+        integer = va_arg(var_arg, int);
+        if (integer < 0)
+            break;
+        if (max < integer)
+            max = integer;
+    }
+    va_end(var_arg);
+    return max;
+}
